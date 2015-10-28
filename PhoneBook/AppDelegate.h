3680aa751679359cf489bb72eeb235d054dc8df7
Gamaley <gamaley@me.com>
2015-10-28 15:55:39 +0200

    Initial Commit

---
+++ PhoneBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhoneBook
+//
+//  Created by Vladyslav on 28.10.15.
+//  Copyright © 2015 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhoneBook/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhoneBook
+//
+//  Created by Vladyslav on 28.10.15.
+//  Copyright © 2015 Vlad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
